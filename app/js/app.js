938ff247256a345e934493532e5e3ddf0438c694
Zhivko Georgiev <zdgeorgiev@abv.bg>
2014-12-30 00:00:56 +0200

    Choosen new theme + amendments in the login & register templates

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,14 @@
 var onlineAdsApp = angular.module('onlineAdsApp', ['ngRoute'])
 	.config(function ($routeProvider) {
-				$routeProvider
+		$routeProvider
 			.when('/login', {
 				templateUrl: 'templates/login.html'
 			})
 			.when('/register', {
 				templateUrl: 'templates/register.html'
+			})
+			.when('/ads', {
+				templateUrl: 'templates/all-ads.html'
 			})
 			.otherwise({redirectTo: '#'});
 	});
