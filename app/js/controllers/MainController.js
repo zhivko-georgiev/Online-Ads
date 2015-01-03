a013740f29ad6e44a4ca120dffb6b9c7bfcdcc7a
Zhivko Georgiev <zdgeorgiev@abv.bg>
2015-01-03 16:37:21 +0200

    Finished initial page with ads + categories & towns

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -2,4 +2,12 @@ onlineAdsApp.controller('MainController', function ($scope, $log, adsData) {
 	adsData.getAds(function(resp) {
 		$scope.data = resp;
 	});
+
+	adsData.getTowns(function(resp) {
+		$scope.towns = resp;
+	});
+
+	adsData.getCategories(function(resp) {
+		$scope.categories = resp;
+	});
 })
--- app/js/services/mainPage.js
+++ app/js/services/mainPage.js
@@ -1,4 +1,4 @@
-onlineAdsApp.factory('adsData', function ($http) {
+onlineAdsApp.factory('adsData', function ($http, $log) {
 	function getAllAds(success) {
 		$http({
 			method: 'GET',
@@ -8,11 +8,39 @@ onlineAdsApp.factory('adsData', function ($http) {
 			success(data, status, headers(), config);
 		})
 		.error(function (data, status, headers, config) {
-			error(data, status, headers(), config);
+			$log.warn(data);
+		});
+	}
+
+	function getAllTowns(success) {
+		$http({
+			method: 'GET',
+			url: 'http://softuni-ads.azurewebsites.net/api/Towns'
+		})
+		.success(function (data, status, headers, config) {
+			success(data, status, headers(), config);
+		})
+		.error(function (data, status, headers, config) {
+			$log.warn(data);
+		});
+	}
+
+	function getAllCategories(success) {
+		$http({
+			method: 'GET',
+			url: 'http://softuni-ads.azurewebsites.net/api/categories'
+		})
+		.success(function (data, status, headers, config) {
+			success(data, status, headers(), config);
+		})
+		.error(function (data, status, headers, config) {
+			$log.warn(data);
 		});
 	}
 
 	return {
 		getAds: getAllAds,
+		getTowns: getAllTowns,
+		getCategories: getAllCategories
 	}
 })
